97c80a0761f0df5e2d8492e9adf8fd01097a3125
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-31 12:35:02 -0400

    add methods to class

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex; 
+    }
+
+    speak() {
+        return(`${this.name} says meow!`)
+    }
+}
+
+let lulu = new Cat('lulu', 'male')
+console.log(lulu.speak())
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex; 
+    }
+    speak() {
+        return(`${this.name} says woof!`)
+    } 
+}
+
+let billy = new Dog('billy', 'male')
+console.log(billy.speak())
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex; 
+    }
+    speak() {
+        if (this.sex === 'male') {
+            return(`It's me! ${this.name}, the parrot!`)
+        } else {
+        return(`${this.name} says squawk!`)
+        }
+    }    
+}
+
+let chilly = new Bird('chilly', 'male')
+console.log(chilly.speak())
